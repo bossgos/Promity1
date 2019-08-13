990e4963f42d323b1f29b38b953d8a8b09bb196b
radoslaw.gosciniak <radoslaw.gosciniak@multizoneit.pl>
2019-08-13 18:01:20 +0200

    Firebase setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCEs7V0A48-2FVlpgcol6dwOW4_CR7gH7E',
+    authDomain: 'promity-82abc.firebaseapp.com',
+    databaseURL: 'https://promity-82abc.firebaseio.com',
+    projectId: 'promity-82abc',
+    storageBucket: '',
+    messagingSenderId: '302699557006',
+    appId: '1:302699557006:web:55795baf60395046'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCEs7V0A48-2FVlpgcol6dwOW4_CR7gH7E',
+    authDomain: 'promity-82abc.firebaseapp.com',
+    databaseURL: 'https://promity-82abc.firebaseio.com',
+    projectId: 'promity-82abc',
+    storageBucket: '',
+    messagingSenderId: '302699557006',
+    appId: '1:302699557006:web:55795baf60395046'
+  }
 };
 
 /*
